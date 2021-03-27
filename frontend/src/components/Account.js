413b83470867f33e0bf827b7bd9e5e016554b2e5
zhuweiji <55840431+zhuweiji@users.noreply.github.com>
2021-03-27 17:26:34 +0800

    added loader for log in added error display for all login functions (login, register, recover pw)

--- frontend/src/components/Account.js
+++ frontend/src/components/Account.js
@@ -3,30 +3,38 @@ import { Link } from 'react-router-dom'
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { contentState } from '../recoil/atoms';
 import { formValuesState, userAuth, authorise, resetPassord } from '../recoil/users'
+import LoadingIcon from '../assets/loader_rings.svg'
 
 export default function Account({view, history}) {
 
   const formValues = useRecoilValue(formValuesState)
   const setUserAuth = useSetRecoilState(userAuth)
   const setPageContent = useSetRecoilState(contentState)
-  const [error, setErrorUI] = useState(false)
+  const [isError, setIsError] = useState(false)
+  const [errorData, setErrorData] = useState('Invalid Credentials');
+  const [sendingRequest, setSendingRequest] = useState(false);
 
   const handleSubmit = async e => {
     e.preventDefault();
+    setSendingRequest(true);
+
     try {
       if (view == "login" || view == "register") {
         // server call
         console.log(formValues);
-        const { data } = await authorise(`/${view}`,formValues) 
-        
-        // show notification
-        // setNotification({
-        //   message: data.message,
-        //   isVisible: true
-        // })
-        // redirect
+
+        let data;
+        try{
+          data = await authorise(`/${view}`,formValues)
+          data = data.data
+        }
+        catch (e) {
+          setIsError(true);
+          setErrorData(Object.values(e.data.data)[0]);
+          console.error(e.data);
+        }
+
         if (data) {
-          // state
           setUserAuth({
             isAuthenticated: true,
             user: data
@@ -37,25 +45,34 @@ export default function Account({view, history}) {
           history.push('/game')
           setPageContent('game')
         }
+
       } else {
-        const res = await resetPassord(formValues.email) 
-        alert(res.message)
-        history.push('/login')
-        setPageContent('login')
+        let res;
+        try{
+          res = await resetPassord(formValues.email)
+          alert(res.message)
+          history.push('/login')
+          setPageContent('login')
+        }catch (e) {
+          setIsError(true);
+          console.error(e)
+          setErrorData('Could not find that account. If you do not have an account, register one instead.')
+        }
+
       }
     } catch (error) {
-      // setNotification({
-      //   message: error.message,
-      //   isVisible: true
-      // })
       console.error(error)
-      setErrorUI(true)
+      setIsError(true)
+      setErrorData(Object.values(e.data.data)[0])
     }
+
+    setSendingRequest(false);
+
   };
 
   return (
     <div className="content account-page" id="login">
-      <div className="login-dialog glow-border" role="dialog" data-submit="joinTeam" data-account-error={error}>
+      <div className="login-dialog glow-border" role="dialog" data-submit="joinTeam" data-account-error={isError}>
         {view == "login" 
           ? <h3 className="dashed">Sign In</h3>
           : view == "register" 
@@ -66,6 +83,9 @@ export default function Account({view, history}) {
         {view == "register" ? <Input label="name" type="text" tips={false}/> : null}
         <Input label="email" type="text" tips={false}/>
         {view != "reset" ? <Input label="password" type="password" tips={false}/> : null}
+        {sendingRequest ? <img src={LoadingIcon} alt="" style={{height:'50px'}}/>: null}
+
+        {isError ? <p>{errorData}</p>:null}
         
         <input className="glow-border" form="login-form" type="submit" value="Enter"/>
         <form id="login-form" onSubmit={handleSubmit}></form>
