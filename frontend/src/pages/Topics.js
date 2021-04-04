5069bc6655c5a953a7b5285e2749bb3ffbfc3532
zhuweiji <55840431+zhuweiji@users.noreply.github.com>
2021-04-04 22:12:13 +0800

    display selected subtopic in topics/page

--- frontend/src/pages/Topics.js
+++ frontend/src/pages/Topics.js
@@ -43,7 +43,8 @@ const Form = ({handleShowForm, isTopic, topic, subtopic, setFormReturn, setReloa
               ):(
                   <div>
                     <h2>Create New Question</h2>
-                    <h4>Selected sub-topic: {subtopic}</h4>
+                    <h4>Selected sub-topic:</h4>
+                    <h4>{subtopic}</h4>
                   </div>
               )}
 
@@ -96,7 +97,8 @@ const EditForm = ({handleShowEditForm,  topic, subtopic, setFormReturn, setReloa
           <div className="sb-task-header dashed" data-click onClick={handleShowEditForm}>
             <div className="game-header-title">
               <h2>Edit Question</h2>
-              <h4>Selected sub-topic: {subtopic}</h4>
+              <h4>Selected sub-topic:</h4>
+              <h4>{subtopic}</h4>
             </div>
           </div>
 
@@ -143,12 +145,13 @@ const DeleteForm = ({handleShowEditForm,  topic, subtopic,setFormReturn, setRelo
           <div className="sb-task-header dashed" data-click onClick={handleShowEditForm}>
             <div className="game-header-title">
               <h2>Delete Question</h2>
-              <h4>Selected sub-topic: {subtopic}</h4>
+              <h4>Selected sub-topic:</h4>
+              <h4>{subtopic}</h4>
             </div>
           </div>
 
           <div>
-            <Input label="question" type="question" formValues={formValues} setFormValues={setFormValues} />
+            <Input label="question" type="question" formValues={formValues} seFormValues={setFormValues} />
           </div>
 
           <input className="glow-border" form="form" type="submit" value="Submit" />
