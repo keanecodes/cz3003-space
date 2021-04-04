449fdb2e02da0b9de2e822b5d5c16e34a3b5cbf4
k <k>
2021-04-05 01:17:04 +0800

    ref error

--- frontend/src/pages/Leaderboard.js
+++ frontend/src/pages/Leaderboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Stats from './Stats'
+import Stats from '../components/Stats'
 
 
 export default function Leaderboard() {
