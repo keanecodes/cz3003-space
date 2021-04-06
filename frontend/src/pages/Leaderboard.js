873cbbfbc8011ed83212d4aedb15e2746658afd4
k <k>
2021-04-06 22:03:22 +0800

    added overview

--- frontend/src/pages/Leaderboard.js
+++ frontend/src/pages/Leaderboard.js
@@ -11,16 +11,17 @@ export default function Leaderboard() {
   const [topic, setTopic] = useState("");
   const [subtopic, setSubtopic] = useState("");
 
-  const [index, setIndex] = useState(0);
+  const [reload, setReload] = useState(false);
   const [showStats, setOptionsOverlay]  = useState(false);
   const [showSummary, setShowSummary]  = useState(false);
 
-  const handleShowStats = () => setOptionsOverlay(!showStats);
+  // const handleShowStats = () => setOptionsOverlay(!showStats);
 
 
 
   useEffect( async () => {
 
+    setReload(false);
     axios.get("/user/get/score")
             .then(data => {
               setUsers(cleanUpScore(data));
@@ -30,14 +31,19 @@ export default function Leaderboard() {
     const top = await getTopics();
     console.log(top[0]);
 
-
+    let sub = [];
     if(topic.length>0)
-      await getSubtopics(topic);
+      sub = await getSubtopics(topic);
     else
-      await getSubtopics(top[0]);
+      sub = await getSubtopics(top[0]);
+    
+    setSubtopic(sub[0])
+
+      
     // console.log(subs)
+    
   
-}, [topic]);
+}, [topic, reload]);
 
 
 
@@ -94,44 +100,60 @@ const getSubtopics = async (topic) => {
 
   const handleTopic = (top) => {
 
+    if(top == "Overview"){
+      setReload(true)
+      setOptionsOverlay(false)
+    }
+    // else
     setTopic(top);
 
   }
 
   const handleSubtopic = (sub) => {
-    setShowSummary(false);
     setSubtopic(sub);
-    handleShowStats();
+    setOptionsOverlay(true);
+    setShowSummary(false);
+    // handleShowStats();
     
   }
 
   const handleSummary = (sub) => {
+    setOptionsOverlay(true);
     setShowSummary(true);
-    handleShowStats();
+    // handleShowStats();
     
   }
 
   return (
     // <div>
     // <div className="game-header">
-
+    <div> 
+    
       <div className="content" role="main" id="teams">
         <div className="sb-table sb-teamlist" data-sort-by="rank" data-unsorted="false">
           <div className="toolbar" justify-content="center">
             <select onChange={(e) => handleTopic(e.target.value)} className="glow-border">
+                <option selected="selected" >Overview</option>
               {
                 topics?.map((top) => (
                   <option>{top}</option>
                 ))
               }
             </select>
-            <select className="glow-border" onChange={(e) => handleSubtopic(e.target.value)}>
-              {
-                subtopics?.map((sub) => (
-                  <option>{sub}</option>
-                ))
-              }
-            </select>
+            {topic === "Overview" ? 
+            null : (
+              <select className="glow-border" onChange={(e) => handleSubtopic(e.target.value)}>
+              <option value="" selected disabled hidden>Select Subtopic</option>
+  
+                {
+                  subtopics?.map((sub) => (
+                    <option>{sub}</option>
+                  ))
+                }
+              </select>
+            )
+            }
+            
 
               {/* <h2 onClick={handleTopic} className="glow-border"><sb-var data-var="id">{topics[index]}</sb-var></h2>
               {
@@ -142,34 +164,39 @@ const getSubtopics = async (topic) => {
               <h2 onClick={handleSummary} className="glow-border"><sb-var data-var="id">Summary</sb-var></h2>            
 
           </div>
-          <div className="sb-table-head">
 
-            <h2>Players</h2>
-            <div className="dashed sb-spacer"/>
-              
-            <h3>Ranking</h3>
-          </div>
-          <div className="sb-table-head">
-            <h4>Place</h4>
-            <h4># Tasks</h4>
-            <h4 className="sb-spacer">Name</h4>
-            <h4>Score</h4>
-          </div>
-          {
-            (users.length > 0) ? users.map((element, index) => (
-              <div key={`${element.username}-${index}`} data-id="PPP" data-sb="true" data-type="sb-team" data-task-summary="17 tasks" data-tasks="crypto-auth,keys-whisperwhileyouwork,misc-distributed,misc-luckyheart,misc-magic,misc-polyglot,misc-secure-safe-1,misc-secure-safe-2,re-drm,re-elisp,re-flagvm,re-journey,re-yawn,web-blind-xss,web-js-safe-3,web-pwn-gdb-as-a-service,web-quinify" title="Solved tasks: crypto-auth,keys-whisperwhileyouwork,misc-distributed,misc-luckyheart,misc-magic,misc-polyglot,misc-secure-safe-1,misc-secure-safe-2,re-drm,re-elisp,re-flagvm,re-journey,re-yawn,web-blind-xss,web-js-safe-3,web-pwn-gdb-as-a-service,web-quinify" data-last="1540747499507" data-score="3350" data-rank="1" data-top100="true" data-top10="true" style={{"--rank":1}} className="sb-team sb-table-row" data-unsorted="true" data-oldrank="1">
-              <span><sb-var data-var="rank">{index+1}</sb-var></span>
-              <span><sb-var data-var="taskSummary">17 tasks</sb-var></span>
-              <span><sb-var data-var="id">{element.username}</sb-var></span>
-              <span><sb-var data-var="score">{element.score}</sb-var></span>
-            </div>
-            )) : <h1> Loading... </h1>
+          {showStats? <Stats topic={topic} subtopic={subtopic} users={users} showSummary={showSummary}/> : (
+            <>
+              <div className="sb-table-head">
+
+                <h2>Players</h2>
+                <div className="dashed sb-spacer"/>
+                  
+                <h3>Ranking</h3>
+              </div>
+              <div className="sb-table-head">
+                <h4>Place</h4>
+                <h4># Tasks</h4>
+                <h4 className="sb-spacer">Name</h4>
+                <h4>Score</h4>
+              </div>
+                {
+                (users.length > 0) ? users.map((element, index) => (
+                <div key={`${element.username}-${index}`} data-id="PPP" data-sb="true" data-type="sb-team" data-task-summary="17 tasks" data-tasks="crypto-auth,keys-whisperwhileyouwork,misc-distributed,misc-luckyheart,misc-magic,misc-polyglot,misc-secure-safe-1,misc-secure-safe-2,re-drm,re-elisp,re-flagvm,re-journey,re-yawn,web-blind-xss,web-js-safe-3,web-pwn-gdb-as-a-service,web-quinify" title="Solved tasks: crypto-auth,keys-whisperwhileyouwork,misc-distributed,misc-luckyheart,misc-magic,misc-polyglot,misc-secure-safe-1,misc-secure-safe-2,re-drm,re-elisp,re-flagvm,re-journey,re-yawn,web-blind-xss,web-js-safe-3,web-pwn-gdb-as-a-service,web-quinify" data-last="1540747499507" data-score="3350" data-rank="1" data-top100="true" data-top10="true" style={{"--rank":1}} className="sb-team sb-table-row" data-unsorted="true" data-oldrank="1">
+                  <span><sb-var data-var="rank">{index+1}</sb-var></span>
+                  <span><sb-var data-var="taskSummary">17 tasks</sb-var></span>
+                  <span><sb-var data-var="id">{element.username}</sb-var></span>
+                  <span><sb-var data-var="score">{element.score}</sb-var></span>
+                </div>
+                )) : <h1> Loading... </h1>
+                }
+              </>
+            )
           }
         </div>
-        <div> 
-            {showStats? <Stats handleShowStats={handleShowStats} topic={topic} subtopic={subtopic} users={users} showSummary={showSummary}/> : null }
-        </div>
+        
       </div>
-    // </div>
+    </div>
   )
+
 }
